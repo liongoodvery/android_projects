879365e2559e2ec09fa83d63c625594648a19962
lion_good <lion_good@163.com>
2016-06-17 20:23:37 +0800

    ad

---
+++ a05lowversion/src/main/java/org/lion/a05lowversion/MyFragment.java
@@ -0,0 +1,22 @@
+package org.lion.a05lowversion;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by more on 2016-06-16 20:41:40.
+ */
+public class MyFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        TextView tv = new TextView(getActivity());
+        tv.setText("ABC");
+        return tv;
+    }
+}
