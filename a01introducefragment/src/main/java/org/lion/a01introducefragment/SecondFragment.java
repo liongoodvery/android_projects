1ace25f2c0bb26c921ad3bba24f7c7d476717061
lion_good <lion_good@163.com>
2016-06-17 20:20:20 +0800

    v1

---
+++ a01introducefragment/src/main/java/org/lion/a01introducefragment/SecondFragment.java
@@ -0,0 +1,19 @@
+package org.lion.a01introducefragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by more on 2016-06-16 18:59:35.
+ */
+public class SecondFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.second_fragment, null);
+    }
+}
---
+++ a02fragment2/src/main/java/org/lion/a02fragment2/FirstFragment.java
@@ -0,0 +1,19 @@
+package org.lion.a02fragment2;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by more on 2016-06-16 18:56:38.
+ */
+public class FirstFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.first_fragment, null);
+    }
+}
